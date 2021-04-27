b8797cadb8268549c59bf0a74e66348f683d6bc2
Michitka Nakatsuji <m391gub@gmail.com>
2021-04-27 16:23:27 +0900

    fixed

--- NLP_100/chapter01/05.py
+++ NLP_100/chapter01/05.py
@@ -2,6 +2,6 @@ def n_gram(n, S):
     return [S[idx:idx + n] for idx in range(len(S) - n + 1)]
 
 S = "I am an NLPer"
-for i in range(1, 4):
-    print(n_gram(i, S))
-    print(n_gram(i, S.split(' ')))
+
+print(n_gram(2, S))
+print(n_gram(2, S.split(' ')))
