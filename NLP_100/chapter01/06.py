05a7ff23de6b05ac0708443cfc8d08c1ac5c3040
Michitka Nakatsuji <m391gub@gmail.com>
2021-04-27 16:30:14 +0900

    fixed

--- NLP_100/chapter01/06.py
+++ NLP_100/chapter01/06.py
@@ -1,8 +1,14 @@
 def n_gram(n, S):
     return [S[idx:idx + n] for idx in range(len(S) - n + 1)]
+
 S1 = 'paraparaparadise'
 S2 = 'paragraph'
 X = n_gram(2, S1)
 Y = n_gram(2, S2)
-print("和集合：{}\n積集合：{}\n差集合：{}".format(set(X) | set(Y), set(X) & set(Y), set(X) - set(Y)))
-print('se' in (set(X) & set(Y)))
+
+union = set(X) | set(Y)
+intersection = set(X) & set(Y)
+diff = set(X) - set(Y)
+
+print("和集合：{}\n積集合：{}\n差集合：{}".format(union, intersection, diff))
+print('se' in (intersection))
