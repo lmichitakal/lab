6d73a1de5e9c1e8116df4944afa932165b27000b
lmichitakal <m391gub@gmail.com>
2021-04-28 16:05:57 +0900

    fixed

--- NLP_100/chapter01/03.py
+++ NLP_100/chapter01/03.py
@@ -1,4 +1,8 @@
-S = "Now I need a drink, alcoholic of course after the heavy lectures involving quantum mechanics"
+S = "Now I need a drink, alcoholic of course after"\
+    "the heavy lectures involving quantum mechanics."
+
+S = S.replace('.', '').replace(',', '')
+
 words = [s for s in S.split(" ")]
 out = [len(word) for word in words]
 print(out)
