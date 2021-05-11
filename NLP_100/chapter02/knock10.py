1cc03c2c6e352174736049981518c8f389f1d72c
Michitka Nakatsuji <m391gub@gmail.com>
2021-05-11 15:08:30 +0900

    add

---
+++ NLP_100/chapter02/knock10.py
@@ -0,0 +1,3 @@
+path = "/Users/michitaka/lab/NLP_100/chapter02/popular-names.txt"
+with open(path, 'r') as f:
+    print(len(f.readlines()))
---
+++ NLP_100/chapter02/knock11.py
@@ -0,0 +1,3 @@
+path = "/Users/michitaka/lab/NLP_100/chapter02/popular-names.txt"
+with open('popular-names.txt', 'r') as f:
+    print(len(f.readlines()))
